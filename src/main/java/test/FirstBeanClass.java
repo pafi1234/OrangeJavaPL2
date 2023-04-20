0c805e86c4fbb84ba5569a5bf9e31706a06f52af
patrycja <patrycja0figat@gmail.com>
2023-04-20 18:07:20 +0200

    [add] MyFirstRestController

---
+++ src/main/java/test/FirstBeanClass.java
@@ -0,0 +1,14 @@
+//package test;
+//
+//import org.springframework.context.annotation.Bean;
+//    @Bean
+//    public class FirstBeanClass {
+//        //Singleton
+//    /*
+//    @Autowired
+//    FirstBeanCLass class;
+//    -Singleton -> single instance
+//    -Prototype -> new instance with each bean injecting
+//    -per HttpSession -< new instance with each http session
+//     */
+//}
